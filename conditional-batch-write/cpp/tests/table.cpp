24564a918dcabb08d842b975b4cfc918cab6897f
Topher <topher@treode.com>
2015-06-08 05:00:58 -0700

    Setup table tests for sharing among multiple table implementations.

--- conditional-batch-write/cpp/tests/table.cpp
+++ conditional-batch-write/cpp/tests/table.cpp
@@ -1,16 +1,18 @@
-#include <cstdint>
-#include <iostream>
-#include <map>
-#include <unordered_map>
+#include <functional>
+#include <memory>
+#include <thread>
 #include <vector>
 
+#include "ConditionLock.hpp"
 #include "CppUnorderedMapOfMap.hpp"
 #include "catch.hpp"
+#include "lock.hpp"
 #include "table.hpp"
 
-using std::cout;
-using std::endl;
+using std::function;
 using std::initializer_list;
+using std::thread;
+using std::unique_ptr;
 using std::vector;
 
 void write(Table &table, uint32_t time, initializer_list<Row> rows) {
@@ -21,7 +23,7 @@ void expect(Table &table, uint32_t time, initializer_list<int> keys, initializer
   REQUIRE(table.read(time, vector<int> (keys)) == vector<Value> (expected));
 }
 
-void expectMoneyConserved(const CppUnorderedMapOfMap &table) {
+void expectMoneyConserved(const Table &table) {
   std::map<uint32_t, vector<Row>> history;
   for (auto c : table.scan())
     history[c.t].push_back(Row(c.k, c.v));
@@ -36,33 +38,45 @@ void expectMoneyConserved(const CppUnorderedMapOfMap &table) {
   }
 }
 
-TEST_CASE("A table should read 0 for any key", "[table]") {
-  CppUnorderedMapOfMap table;
-  expect(table, 0, {0}, {Value(0, 0)});
-  expect(table, 1, {0}, {Value(0, 0)});
+void table_behaviors(const function<Table*(void)> &new_table) {
+
+  size_t nbrokers = 8;
+  size_t ntransfers = 1000;
+
+  SECTION("A table should read 0 for any key", "[table]") {
+    unique_ptr<Table> table (new_table());
+    expect(*table, 0, {0}, {Value(0, 0)});
+    expect(*table, 1, {0}, {Value(0, 0)});
+  }
+
+  SECTION("A table should read what was put", "[table]") {
+    unique_ptr<Table> table (new_table());
+    write(*table, 0, {Row(0, 1)});
+    expect(*table, 1, {0}, {Value(1, 1)});
+  }
+
+  SECTION("A table should read and write batches", "[table]") {
+    unique_ptr<Table> table (new_table());
+    write(*table, 0, {Row(0, 1), Row(1, 2)});
+    expect(*table, 1, {0, 1}, {Value(1, 1), Value(2, 1)});
+  }
+
+  SECTION("A table should reject a stale write", "[table]") {
+    unique_ptr<Table> table (new_table());
+    write(*table, 0, {Row(0, 1)});
+    REQUIRE_THROWS_AS(write(*table, 0, {Row(0,2)}), stale_exception);
+    expect(*table, 1, {0}, {Value(1,1)});
+  }
+
+  SECTION("A table should preserve the money supply running serially", "[table]") {
+    unique_ptr<Table> table (new_table());
+    broker(*table, ntransfers);
+    expectMoneyConserved(*table);
+  }
 }
 
-TEST_CASE("A table should read what was put", "[table]") {
-  CppUnorderedMapOfMap table;
-  write(table, 0, {Row(0, 1)});
-  expect(table, 1, {0}, {Value(1, 1)});
-}
-
-TEST_CASE("A table should read and write batches", "[table]") {
-  CppUnorderedMapOfMap table;
-  write(table, 0, {Row(0, 1), Row(1, 2)});
-  expect(table, 1, {0, 1}, {Value(1, 1), Value(2, 1)});
-}
-
-TEST_CASE("A table should reject a stale write", "[table]") {
-  CppUnorderedMapOfMap table;
-  write(table, 0, {Row(0, 1)});
-  REQUIRE_THROWS_AS(write(table, 0, {Row(0,2)}), stale_exception);
-  expect(table, 1, {0}, {Value(1,1)});
-}
-
-TEST_CASE("A table should preserve the money supply running serially", "[table]") {
-  CppUnorderedMapOfMap table;
-  broker(table, 1000);
-  expectMoneyConserved(table);
+TEST_CASE ("The CppUnorderedMapOfMap should work", "[tables]") {
+  table_behaviors([] {
+    return new CppUnorderedMapOfMap();
+  });
 }
